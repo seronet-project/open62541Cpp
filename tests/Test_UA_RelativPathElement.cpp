c03fb9d42ade1e87eed635ffdba02b07b6e69af6
Alexander Uhl <alexander.uhl@isw.uni-stuttgart.de>
2025-04-08 10:05:50 +0200

    fix: Assignment Operator RelativPathElement

--- tests/Test_UA_RelativPathElement.cpp
+++ tests/Test_UA_RelativPathElement.cpp
@@ -3,18 +3,53 @@
 #include <Open62541Cpp/UA_RelativPathElement.hpp>
 
 // Memory leak test
-TEST(RelativPathElement, CreateDetroy)
+TEST(RelativPathElement, CreateDestroy)
 {
-    open62541Cpp::UA_RelativPathElement obj1(1, "dasd");
+    open62541Cpp::UA_RelativPathElement obj1(1, "test");
     open62541Cpp::UA_RelativPathElement obj2;
     open62541Cpp::UA_RelativPathElement obj3(obj1);
 }
 
 TEST(RelativPathElement, Assign)
 {
-    open62541Cpp::UA_RelativPathElement obj1(1, "dasd");
+    open62541Cpp::UA_RelativPathElement obj1(1, "test");
     open62541Cpp::UA_RelativPathElement obj2;
     obj2 = obj1;
     open62541Cpp::UA_RelativPathElement obj3 = obj1;
 }
 
+TEST(RelativPathElement, AssignNotNullToNull)
+{
+    open62541Cpp::UA_RelativPathElement obj1(1, "test");
+    open62541Cpp::UA_RelativPathElement obj2;
+    obj1 = obj2;
+}
+
+TEST(RelativPathElement, AssignNotNullToNotNull)
+{
+    open62541Cpp::UA_RelativPathElement obj1(1, "test");
+    open62541Cpp::UA_RelativPathElement obj2(2, "test2");
+    obj1 = obj2;
+
+}
+
+TEST(RelativPathElement, AssignNullToNull)
+{
+    open62541Cpp::UA_RelativPathElement obj1;
+    open62541Cpp::UA_RelativPathElement obj2;
+    obj1 = obj2;
+}
+
+TEST(RelativPathElement, AssignNullToNotNull)
+{
+    open62541Cpp::UA_RelativPathElement obj1(1, "test");
+    open62541Cpp::UA_RelativPathElement obj2;
+    obj1 = obj2;
+}
+
+TEST(RelativPathElement, MemoryLeakTest)
+{
+    char* str = new char[30];
+    str = new char[60];
+    delete[] str;
+}
